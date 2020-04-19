11cfae23688a6327fa59031a29acce3dddc0c883
treylavergne <treylavergne@gmail.com>
2020-04-19 18:38:35 -0500

    Done.

--- spec/04_querying_spec.rb
+++ spec/04_querying_spec.rb
@@ -7,7 +7,7 @@ describe 'querying' do
   end
 
   it 'selects all of the books titles and years in the first series and orders them chronologically' do
-    expect(@db.execute(select_books_titles_and_years_in_first_series_order_by_year)).to eq([["Game of Thrones", 1996], ["A Clash of Kings", 1998], ["A Storm of Swords", 2000]])
+    expect(@db.execute(select_books_titles_and_years_in_first_series_order_by_year)).to eq([["Game of Thrones", "1996"], ["A Clash of Kings", "1998"], ["A Storm of Swords", "2000"]])
   end
 
   it 'returns the name and motto of the character with the longest motto' do
